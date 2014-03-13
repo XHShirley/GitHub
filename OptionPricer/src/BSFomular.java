b60af3e4105668aea0379ea38af910ef2cf94019
reiliuyan <reiliuyan@gmail.com>
2014-03-13 22:28:21 +1030

    Try again

---
+++ OptionPricer/src/BSFomular.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Yan Liu
+ *
+ */
+import java.lang.Math
+
+public class BSFomular {
+	StandardNormal standardNormal;
+	double d1;
+	double d2;
+	
+	public BSFomular(){
+		standardNormal = new StandardNormal;
+	}
+	
+	public double calculate(double s, double k, double t, double r, double v){
+		double result;
+		d1= Math
+		return result;
+	}
+}
